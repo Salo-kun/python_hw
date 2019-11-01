661eef64312c543e81ede7b135c5ae7f663e4c26
Salo-kun <andrii.avramuk@gmail.com>
2019-11-01 13:41:24 +0200

    optimization of second task

--- l1_hw/Second task.py
+++ l1_hw/Second task.py
@@ -1,7 +1,4 @@
 name=input("What is your name? ")
 age=input("How old are you? ")
 city=input("Where do you live? ")
-message_name= 'Hello, {}.'
-message_age='Your age is {}.'
-message_city='You live in {}.'
-print(message_name.format(name), message_age.format(age), message_city.format(city))
+print("Hello, {}. You'r age is {}. You live in {}.".format(name, age, city))
