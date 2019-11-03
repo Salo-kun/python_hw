6f01b9b4f921bfff717305f689fd11fccc478bbd
Avramuk Andrii <avramuk.andrii@gmail.com>
2019-11-03 16:00:11 +0200

    add cw solution

---
+++ Class works/11-01-19.py
@@ -0,0 +1,104 @@
+##########################################task1
+
+# spusok = list(range(0,100,2))
+# print(spusok)
+
+################################task1.2
+
+#num = []
+
+#for a in range (1,101):
+#    if a%2 == 0:
+#        num.append(a)
+
+
+#print(num)
+    
+
+#numer = []
+#count = 0
+#while count <= 100:
+#    count += 1
+#    if count%2 == 0:
+#        numer.append(count)
+
+
+#print(numer)
+    
+##################################task 2
+
+#num = []
+
+#for a in range (1,101):
+#    if a%2 == 1:
+#        num.append(a)
+
+
+#print(num)
+    
+
+#numer = []
+#count = 0
+#while count <= 100:
+#    count += 1
+#    if count%2 == 1:
+#        numer.append(count)
+
+
+#print(numer)
+
+
+################################task3
+
+#numberl = (8, 2, 6, 10, 12, 12)
+#for a in numberl:
+#    if a % 2 != 0:
+#        print("Odd number")
+#        break
+#else:
+#    print("All numbers is even") 
+
+
+#################################task4
+
+#i = 0
+#islist = [1,100]
+#while i < len(islist):
+#    islist[i] = float(islist[i])
+#    i += 1
+#print(islist)
+
+
+#################################task5
+
+#n = int(input('Input num for start: '))
+#n1 = 0
+#n2 = 1
+#i = 0
+#while i < n:
+#     summary = n1 + n2
+#     print(n1)
+#     n2 = n1
+#     n1 = summary
+#     i += 1
+
+################################task6
+
+
+#word = ['cont', 'folk', 'synth', 'jump']
+#for step in word:
+#    print(step)
+
+
+###############################task7
+
+#wordlist = ['cont', 'folk', 'synth', 'jump']
+#for step in wordlist:
+#    for char in step:
+#        print(char, end = '#')
+
+
+##############################task8
+
+numb = input('Insert number ')
+    
