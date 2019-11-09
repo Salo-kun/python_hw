561e6a9c4ac8650b366f7c10c950c09095de828f
Avramuk Andrii <avramuk.andrii@gmail.com>
2019-11-09 21:56:47 +0200

    add new codewars

--- Code Wars/banjo.py
+++ Code Wars/banjo.py
@@ -1,4 +1,4 @@
 def areYouPlayingBanjo(name):
     if name[0].lower() == 'r':
         return "{} plays banjo".format(name)
-    return "{} does not play banjo".format(name)s
+    return "{} does not play banjo".format(name)
